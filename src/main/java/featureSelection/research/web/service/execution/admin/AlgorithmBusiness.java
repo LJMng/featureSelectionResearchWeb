97799be90c2ef0bf6378b53e1ecbd0b50a6a8630
ccjmlkj-coffee <1009710828@qq.com>
2020-05-12 19:53:20 +0800

    execution整合demo系统完成算法参数添加

--- src/main/java/featureSelection/research/web/service/execution/admin/AlgorithmBusiness.java
+++ src/main/java/featureSelection/research/web/service/execution/admin/AlgorithmBusiness.java
@@ -1,6 +1,7 @@
 package featureSelection.research.web.service.execution.admin;
 
 import featureSelection.research.web.entity.execution.admin.Algorithm;
+import featureSelection.research.web.entity.execution.admin.ParameterInfo;
 import featureSelection.research.web.entity.execution.admin.ProcedureSettings;
 
 import java.util.List;
@@ -10,4 +11,6 @@ public interface AlgorithmBusiness {
     public void createParamSettingInfo(Map<Integer, String> algorithmMap);
     public void addProcedureSettings(ProcedureSettings procedureSettings);
     public List<Algorithm> getAlgorithms();
+
+    public void createParameters(ParameterInfo parameterInfo);
 }
