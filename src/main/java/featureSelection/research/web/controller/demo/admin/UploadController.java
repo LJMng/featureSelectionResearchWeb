75f0647056db0dd2e7c3f89a73cfd2d0b1f260de
poissa <975105502@qq.com>
2020-05-27 16:14:07 +0800

    [修改] 再次添加忘记上传的代码

--- src/main/java/featureSelection/research/web/controller/demo/admin/UploadController.java
+++ src/main/java/featureSelection/research/web/controller/demo/admin/UploadController.java
@@ -1,14 +1,17 @@
 package featureSelection.research.web.controller.demo.admin;
 
-import featureSelection.research.web.entity.demo.admin.HtmlElementDemoAdmin;
 import featureSelection.research.web.mybatisMapper.demo.admin.HtmlElementDemoAdminMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 /**
  * @author jjz
@@ -17,23 +20,18 @@ import java.util.UUID;
 public class UploadController {
 
     //改为项目地址
-//    private static String UPLOAD_FOLDER = "C:\\Users\\jiang\\Desktop\\featureSelection - 副本 - 副本 - 副本\\src\\main\\resources\\public\\demoAdmin\\images\\";
-    private static String UPLOAD_FOLDER = "classpath://demoAdmin/images/";
+    private static String UPLOAD_FOLDER = "C:\\Users\\jiang\\Desktop\\featureSelection2\\src\\main\\resources\\public\\images\\";
 
     @Autowired
     HtmlElementDemoAdminMapper htmlElementDemoAdminMapper;
 
-    /**
-     * @param description
-     *      前端返回的图片附带的信息
-     * @param file
-     *      前端返回的图片
-     * @return 返回关于我们管理页面
-     * */
-    @PostMapping("/formUpload")
-    public String uploadForm(@RequestParam("description") String description,
-                             @RequestParam("file") MultipartFile file){
-        try {
+    @ResponseBody
+    @RequestMapping("/img")
+    public Map<String,Object> uploadImgQiniu(@RequestParam("files") MultipartFile
+                                                     file) throws IOException {
+        Map<String,Object> map = new HashMap<String,Object>();
+        String returnName = null;
+        if(!file.isEmpty()) {
             //获取后缀
             String extension = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
 
@@ -42,20 +40,25 @@ public class UploadController {
             String str = uuid.toString();
             String imageName = str.substring(0,8)+str.substring(9,13)+str.substring(14, 18)+str.substring(19, 23)+str.substring(24);
             String relativeAddr = imageName + extension;
+            returnName = relativeAddr;
 
-            //保存图片
-            File image = new File(UPLOAD_FOLDER+relativeAddr);
-            file.transferTo(image);
+            // fileName处理
+            relativeAddr = UPLOAD_FOLDER + relativeAddr;
+            // 文件对象
+            File dest = new File(relativeAddr);
+            // 创建路径
+            if(!dest.getParentFile().exists()){
+                dest.getParentFile().mkdir();
+            }
+            try {
+                //保存文件
+                file.transferTo(dest);
 
-            //设置要保存的页面元素并存入数据库
-            HtmlElementDemoAdmin e = new HtmlElementDemoAdmin();
-            e.setModuleKey("aboutusimage"+str.substring(0,8));
-            e.setEnValue("/images/"+relativeAddr);
-            e.setChValue(description);
-            htmlElementDemoAdminMapper.saveImage(e);
-        } catch (Exception e) {
-            e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        return "redirect:/DemoAdmin/AboutusAdmin";
+        map.put("filename", "<img src=\"/images/"+returnName+"\" alt=\"...\" class=\"card-img-top\">");
+        return map;
     }
 }
