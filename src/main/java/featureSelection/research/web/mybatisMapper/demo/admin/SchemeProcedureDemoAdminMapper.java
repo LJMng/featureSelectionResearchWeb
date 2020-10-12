c3c46acbcf2f1ea61805c1637aae89534e3186d5
poissa <975105502@qq.com>
2020-10-12 18:18:47 +0800

    [修改] 修改了方案管理步骤显示错误的bug

--- src/main/java/featureSelection/research/web/mybatisMapper/demo/admin/SchemeProcedureDemoAdminMapper.java
+++ src/main/java/featureSelection/research/web/mybatisMapper/demo/admin/SchemeProcedureDemoAdminMapper.java
@@ -1,5 +1,6 @@
 package featureSelection.research.web.mybatisMapper.demo.admin;
 
+import featureSelection.research.web.entity.demo.admin.ProcedureSettingsDemoAdmin;
 import featureSelection.research.web.entity.demo.admin.SchemeDemoAdmin;
 import featureSelection.research.web.entity.demo.admin.SchemeProcedureDemoAdmin;
 import org.apache.ibatis.annotations.Mapper;
@@ -17,6 +18,7 @@ public interface SchemeProcedureDemoAdminMapper {
     //增加步骤(步骤被删除后)
     public List<SchemeProcedureDemoAdmin> findSchemeProcedureAfterDeleteSchemeId();
     public int insertSchemeProcedureAfterDeleteDemoAdmin(SchemeProcedureDemoAdmin schemeProcedureDemoAdmin);
+    public ProcedureSettingsDemoAdmin findNewProcedureAlgorithmId();
     //更新被绑定ID的步骤
     public int updateSchemeProcedureDemoAdmin(List<SchemeProcedureDemoAdmin> list);
     //删除被绑定ID的步骤
